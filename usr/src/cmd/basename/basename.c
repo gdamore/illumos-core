4eb801ccad7315c0e627339261669bd97b035a3d
Garrett D'Amore <garrett@damore.org>
2014-08-22 14:13:11 -0700

    fixes #33 too many versions of basename

--- usr/src/cmd/basename/basename.c
+++ usr/src/cmd/basename/basename.c
@@ -20,21 +20,16 @@
  * CDDL HEADER END
  */
 /*
+ * Copyright 2014 Garrett D'Amore <garrett@damore.org>
+ *
  * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
 
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifndef	XPG4
-#include <unistd.h>
-#include <regex.h>
-#include <libintl.h>
-#endif
 
 int
 main(int argc, char **argv)
@@ -42,14 +37,6 @@ main(int argc, char **argv)
 	char	*p;
 	char	*string;
 	char	*suffix;
-#ifndef	XPG4
-	int	r;
-	char	suf_buf[256];
-	char	*suf_pat;
-	size_t	suf_len;
-	regex_t	reg;
-	regmatch_t	pmatch[2];
-#endif
 
 	/*
 	 * For better performance, defer the setlocale()/textdomain()
@@ -63,7 +50,6 @@ main(int argc, char **argv)
 		return (0);
 	}
 
-#ifdef	XPG4
 	if (strcmp(argv[1], "--") == 0) {
 		argv++;
 		argc--;
@@ -72,7 +58,7 @@ main(int argc, char **argv)
 			return (0);
 		}
 	}
-#endif
+
 	if (argc > 3) {
 		(void) setlocale(LC_ALL, "");
 		(void) textdomain(TEXT_DOMAIN);
@@ -108,7 +94,6 @@ main(int argc, char **argv)
 		return (0);
 	}
 
-#ifdef	XPG4
 	/*
 	 * if a suffix is present and is not the same as the remaining
 	 * string and is identical to the last characters in the remaining
@@ -121,51 +106,4 @@ main(int argc, char **argv)
 	}
 	(void) puts(string);
 	return (0);
-#else
-	(void) setlocale(LC_ALL, "");
-	(void) textdomain(TEXT_DOMAIN);
-
-	suf_len = 6 + strlen(suffix) + 1 + 1; /* \(.*\)suffix$ */
-	if (suf_len > sizeof (suf_buf)) {
-		suf_pat = malloc(suf_len);
-		if (suf_pat == NULL) {
-			(void) fputs("malloc failed\n", stderr);
-			return (1);
-		}
-	} else {
-		suf_pat = suf_buf;
-	}
-	(void) strcpy(suf_pat, "\\(.*\\)");
-	(void) strcpy(suf_pat + 6, suffix);
-	*(suf_pat + suf_len - 1 - 1) = '$';
-	*(suf_pat + suf_len - 1) = '\0';
-
-	r = regcomp(&reg, suf_pat, 0);
-	if (r != 0) {
-		(void) fprintf(stderr,
-		    "Internal error: regcomp failed for \"%s\"\n",
-		    suf_pat);
-		return (1);
-	}
-	r = regexec(&reg, string, 2, pmatch, 0);
-	if (r == 0) {
-		if (pmatch[0].rm_so == (regoff_t)-1 ||
-		    pmatch[1].rm_so == (regoff_t)-1 ||
-		    pmatch[1].rm_so != 0) {
-			(void) fprintf(stderr, "Internal error: regexec did "
-			    "not set sub-expression for:\n");
-			(void) fprintf(stderr, "path: \"%s\"\n", string);
-			(void) fprintf(stderr, "pattern: \"%s\"", suf_pat);
-			return (1);
-		}
-		if (pmatch[1].rm_so == pmatch[1].rm_eo) {
-			/* a null string matched */
-			(void) printf("%s\n", string);
-			return (0);
-		}
-		string[pmatch[1].rm_eo] = '\0';
-	}
-	(void) puts(string);
-	return (0);
-#endif
 }
